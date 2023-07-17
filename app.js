2e7f87d82a0ed6106adf8e68993cadae4e82e476
Harsh <harshmkale.2004@gmail.com>
2023-07-17 08:08:49 +0530

    Chat Functionality added and the old chats load added templating also working now app runs successfully git add .git add .!

--- app.js
+++ app.js
@@ -14,16 +14,52 @@ const http = require('http').Server(app);
 const port = 3000;
 
 const userRoute = require('./routes/userRoute');
+const User = require("./models/userModel");
+const Chat = require("./models/chatModel");
 
 app.use('/', userRoute);
 
-// const io = require("socket.io")(http);
+const io = require("socket.io")(http);
 
-// var usp = io.of('/user-namespace');
+var usp = io.of('/user-namespace');
 
-// usp.on();
 
-http.listen(port, function(){
+usp.on("connection", async function (socket) {    
+    var userId = socket.handshake.auth.token;
+    await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: '1' } });
+    // User Status Broadcast
+    socket.broadcast.emit("getOnlineUser", { user_id: userId });
+    console.log("User Connected!");
+    socket.on("disconnect", async function () {
+        var time = Date();
+        var activeAt = time.substr(0, 15);
+        console.log("User Disconnected!");
+        var userId = socket.handshake.auth.token;
+        await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: '0' } });
+        // User Status Broadcast
+        socket.broadcast.emit("getOfflineUser", { user_id: userId, time: activeAt });
+    });
+    // chatting implementation
+    socket.on("newChat", function(data){
+        socket.broadcast.emit("loadNewChat", data);
+    });
+    // load old chats
+    socket.on("existsChat", async function(data){
+        var chats = await Chat.find({ $or: [
+            {
+                sender_id: data.sender_id,
+                receiver_id: data.receiver_id
+            },
+            {
+                sender_id: data.receiver_id,
+                receiver_id: data.sender_id
+            }
+        ] });
+        socket.emit("loadChats", { chats: chats });
+    });
+});
+
+http.listen(port, function () {
     console.log(`Server is running at http://localhost:${port}`);
 })
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const Chat = require("../models/chatModel");
 const bcrypt = require('bcrypt');
 
 const registerLoad = async(req, res) =>{
@@ -37,23 +38,23 @@ const login = async(req, res) => {
     try {
         const email = req.body.email;
         const password = req.body.password;
-        // Checking in the db is current email and password exists!
-        const userData = await User.findOne({ email: email });
+
+        const userData = await User.findOne({email: email});
         if(userData){
-            const passwordMatch = bcrypt.compare(password, userData.password);
+            const passwordMatch = await bcrypt.compare(password, userData.password);
             if(passwordMatch){
                 req.session.user = userData;
-                res.redirect("/dashboard");
+                res.redirect('/dashboard');
             }else{
-                res.render("login", { message: "Email & Password is incorrect!" });
+                res.render('login', { message: "Email and Password is Incorrect!" });
             }
         }else{
-            res.render("login", { message: "Email & Password is Incorrect!" });
+            res.render('login', { message: "Email and Password is Incorrect!" });
         }
     } catch (error) {
         console.log(error.message);
     }
-};
+}
 
 const logout = async(req, res) => {
     try {
@@ -81,6 +82,30 @@ const loadChatPg = async(req, res) => {
     }
 }
 
+const saveChat = async(req, res) => {
+    try {
+        var chat = new Chat({
+            sender_id: req.body.sender_id,
+            receiver_id: req.body.receiver_id,
+            message: req.body.message 
+        });
+
+        var newChat = await chat.save();
+
+        res.status(200).send({
+            success: true,
+            msg: "Chat Saved Successfully & sent!",
+            data: newChat
+        });
+
+    } catch (error) {
+        res.status(400).send({
+            success: false,
+            msg: error.message
+        });
+    }
+}
+
 module.exports = {
     registerLoad,
     register,
@@ -88,5 +113,6 @@ module.exports = {
     login,
     logout,
     loadChatPg,
+    saveChat,
     loadDashboard
 }
